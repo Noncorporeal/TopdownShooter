5476000ae22a20fe8960ae2009a1967971d138dc
PhantomTissue <spongetoph@gmail.com>
2021-03-01 00:05:30 -0700

    New Generator Basis
    Spawns in objects, and rooms, nothing else.

--- Assets/Resources/Scripts/Level Gen Scripts/LevelManager.cs
+++ Assets/Resources/Scripts/Level Gen Scripts/LevelManager.cs
@@ -5,57 +5,25 @@ using System;
 
 public class LevelManager : MonoBehaviour
 {
-    [RangeAttribute(5,20)]
+    [RangeAttribute(15,30)]
     public int MapSize;
     public int seed;//8 Digit Min/Max
-    private int roomCount;
-    private int[,] roomPoints;
+    public int roomCount;
+
 
     public GameObject _wall;
     public GameObject _floor;
-
-    public class Tile
-    {
-        public bool up;
-        public bool down;
-        public bool left;
-        public bool right;
-        //false = wall exists. True = no wall;
-
-        public bool used;
-        public bool isRoom;
-
-        public Tile()
-        {
-            up = false;
-            down = false;
-            left = false;
-            right = false;
-            used = false;
-            isRoom = false;
-        }
-
-        public void RoomPrep()
-        {
-            up = true;
-            down = true;
-            left = true;
-            right = true;
-            used = true;
-            isRoom = true;
-        }
-    }
 
     enum direction
     {
         up, down, left, right
     }
 
-    public Tile[,] MapMatrix;
+    public int[,] MapMatrix;
 
     private void Start()
     {
-        MapMatrix = new Tile[MapSize, MapSize];
+        MapMatrix = new int[MapSize, MapSize];
         System.Random rand;
         if (seed != 0)
         {
@@ -84,205 +52,36 @@ public class LevelManager : MonoBehaviour
 
     private void InitializeMatrix()
     {
-        for (int i = 0; i < MapSize; i++)
+        for (int i = 0; i <= MapSize - 1; i++)
         {
-            for (int j = 0; j < MapSize; j++)
+            for (int j= 0; j <= MapSize - 1; j++)
             {
-                MapMatrix[i, j] = new Tile();
+                MapMatrix[i, j] = 0;
             }
         }
     }
 
     private void SpawnRooms(ref System.Random rand)
     {
-
-        
-        if (MapSize < 9)
+        for (int i = 0; i <= roomCount; i++)
         {
-            roomCount = MapSize - 6;
-            if (roomCount <= 0)
-                return;
-        }
-        else if (MapSize == 9 || MapSize == 10)
-        {
-            roomCount = 3;
-        }
-        else if (MapSize >= 11 && MapSize <= 13)
-        {
-            roomCount = rand.Next(3, 5);
-        }
-        else if (MapSize >= 14 && MapSize <= 16)
-        {
-            roomCount = rand.Next(3, 6);
-        }
-        else if (MapSize >= 17 && MapSize <= 20)
-        {
-            roomCount = rand.Next(4, 7);
-        }
-        //Each 2 points represents upper-left and lower-right corner of room
-
-        int[] pointA = new int[2];
-        int[] pointB = new int[2];
-
-        System.Random r = new System.Random(seed);
-
-        for (int x = 0; x < roomCount; x++)
-        {
-            pointA[0] = rand.Next(0, MapSize - 2);
-            pointA[1] = rand.Next(0, MapSize - 2);
-            pointB[0] = rand.Next(pointA[0] + 1, pointA[0] + 4);
-            pointB[1] = rand.Next(pointA[1] + 1, pointA[1] + 4);
-
-            if (pointB[0] >= MapSize)
+            int roomSizeX= rand.Next(1, (int)MapSize / 4);
+            int roomSizeY = rand.Next(1, (int)MapSize / 4);
+            int roomPointX = rand.Next(1, MapSize - roomSizeX);
+            int roomPointY = rand.Next(1, MapSize - roomSizeY);
+            for (int j = roomPointX; j <= roomPointX + roomSizeX; j++)
             {
-                pointB[0] = MapSize - 1;
-            }
-            if (pointB[1] >= MapSize)
-            {
-                pointB[1] = MapSize - 1;
-            }
-
-            for (int j = pointA[1]; j <= pointB[1]; j++)
-            {
-                for (int i = pointA[0]; i <= pointB[0]; i++)
+                for (int k = roomPointY; k <= roomPointY + roomSizeY; k++)
                 {
-                    MapMatrix[i, j].RoomPrep();
-                    
-                    if (i == pointA[0])
-                    {
-                        if (r.Next(3) != 0)
-                            MapMatrix[i, j].left = false;
-                    }
-                    if (i == pointB[0])
-                    {
-                        if (i + 1 != MapSize)
-                            if (r.Next(3) != 0)
-                                MapMatrix[i + 1, j].left = false;
-                        if (r.Next(3) != 0)
-                            MapMatrix[i, j].right = false;
-                    }
-                    if (j == pointA[1])
-                    {
-                        if (r.Next(3) != 0)
-                            MapMatrix[i, j].up = false;
-                    }
-                    if (j == pointB[1])
-                    {
-                        if (j + 1 != MapSize)
-                            if (r.Next(3) != 0)
-                                MapMatrix[i, j + 1].up = false;
-                        if (r.Next(3) != 0)
-                            MapMatrix[i, j].down = false;
-                    }
-                    
+                    MapMatrix[j, k] = 1;
                 }
             }
         }
     }
 
-    private void SpawnMainPath(ref System.Random rand, int locX, int locY)//DONE, DONT TOUCH
+    private void SpawnMainPath(ref System.Random rand, int locX, int locY)
     {
-        /*
-         * This section is responsible for creating a maze. It will not spawn a maze around rooms.
-         */
-        MapMatrix[locX, locY].used = true;
-        List<direction> directions = new List<direction>();
-        //Check left
-        if (locX != 0)
-        {
-            if (MapMatrix[locX - 1, locY].used == false)
-            {
-                directions.Add(direction.left);
-            }
-        }
-        //Check right
-        if (locX != MapSize - 1)
-        {
-            if (MapMatrix[locX + 1, locY].used == false)
-            {
-                directions.Add(direction.right);
-            }
-        }
-        //check up
-        if (locY != 0)
-        {
-            if (MapMatrix[locX, locY - 1].used == false)
-            {
-                directions.Add(direction.up);
-            }
-        }
-        //check down
-        if (locY != MapSize - 1)
-        {
-            if (MapMatrix[locX, locY + 1].used == false)
-            {
-                directions.Add(direction.down);
-            }
-        }
-
-        //check if direction list is empty, also removes us from function at end.
-        if (directions.Count == 0)
-            return;
-
-        //as long as there are more directions, keep filling them.
-        while (directions.Count != 0)
-        {
-            
-            int rng = rand.Next(0, 100);
-            rng = rng % (directions.Count);
-            switch (directions[rng])
-            {
-                case direction.up:
-                    //double-check that direction hasnt been used yet
-                    if (MapMatrix[locX, locY - 1].used)
-                    {
-                        directions.Remove(direction.up);
-                        break;
-                    }
-                    //remove walls between cells
-                    MapMatrix[locX, locY].up = true;
-                    MapMatrix[locX, locY - 1].down = true;
-                    //move to new cell and repeat
-                    SpawnMainPath(ref rand, locX, locY - 1);
-                    //remove direction from choices.
-                    directions.Remove(direction.up);
-                    break;
-                case direction.down:
-                    if (MapMatrix[locX, locY + 1].used)
-                    {
-                        directions.Remove(direction.down);
-                        break;
-                    }
-                    MapMatrix[locX, locY].down = true;
-                    MapMatrix[locX, locY + 1].up = true;
-                    SpawnMainPath(ref rand, locX, locY + 1);
-                    directions.Remove(direction.down);
-                    break;
-                case direction.left:
-                    if (MapMatrix[locX - 1, locY].used)
-                    {
-                        directions.Remove(direction.left);
-                        break;
-                    }
-                    MapMatrix[locX, locY].left = true;
-                    MapMatrix[locX - 1, locY].right = true;
-                    SpawnMainPath(ref rand, locX - 1, locY);
-                    directions.Remove(direction.left);
-                    break;
-                case direction.right:
-                    if (MapMatrix[locX + 1, locY].used)
-                    {
-                        directions.Remove(direction.right);
-                        break;
-                    }
-                    MapMatrix[locX, locY].right = true;
-                    MapMatrix[locX + 1, locY].left = true;
-                    SpawnMainPath(ref rand, locX + 1, locY);
-                    directions.Remove(direction.right);
-                    break;
-            }
-        }
-        return;
+       
     }
 
     private void SpawnEndPoints(ref System.Random rand) //TODO
@@ -290,78 +89,38 @@ public class LevelManager : MonoBehaviour
 
     }
 
-    private void PopulateMap(ref System.Random rand)//DONE, DONT TOUCH
+    private void PopulateMap(ref System.Random rand)
     {
-        Vector3 upLoc = new Vector3(0, 0, 0);  
-        Vector3 downLoc = new Vector3(5, 0, 0);
-        Vector3 leftLoc = new Vector3(0, 0, 0);
-        Vector3 rightLoc = new Vector3(0, 0, 5);
-        Vector3 floorLoc = new Vector3(0, 0, 0);
-        GameObject mapMaster = new GameObject("Map Master");
+        Vector3 wallSpawn = new Vector3(2.5f, 2.5f, 2.5f);
+        Vector3 floorSpawn = new Vector3(0, 0, 0);
+        GameObject MapMaster = new GameObject("MapMaster");
 
-        for (int j = 0; j < MapSize; j++)
+        for (int j = 0; j <= MapSize - 1; j++)
         {
-            for (int i = 0; i < MapSize; i++)
+            for (int i = 0; i<= MapSize - 1; i++)
             {
-                GameObject floor = GameObject.Instantiate(_floor);
-                floor.transform.position = floorLoc;
-                floor.transform.parent = mapMaster.transform;
-                if (!MapMatrix[i,j].left)
+                if (MapMatrix[i,j] == 0)
                 {
-                    GameObject leftWall = GameObject.Instantiate(_wall);
-                    leftWall.transform.position = leftLoc;
-                    leftWall.transform.parent = mapMaster.transform;
-                    leftWall.layer = 10;
+                    GameObject wall = GameObject.Instantiate(_wall);
+                    wall.transform.position = wallSpawn;
+                    wall.transform.parent = MapMaster.transform;
+                    wallSpawn.z += 5;
+                    floorSpawn.z += 5;
                 }
-                if (!MapMatrix[i,j].up)
+                else if (MapMatrix[i,j] == 1)
                 {
-                    GameObject upWall = GameObject.Instantiate(_wall);
-                    upWall.transform.position = upLoc;
-                    upWall.transform.rotation = Quaternion.Euler(0,-90,0);
-                    upWall.transform.parent = mapMaster.transform;
-                    upWall.layer = 10;
+                    GameObject floor = GameObject.Instantiate(_floor);
+                    floor.transform.position = floorSpawn;
+                    floor.transform.parent = MapMaster.transform;
+                    wallSpawn.z += 5;
+                    floorSpawn.z += 5;
                 }
-                if(i == MapSize - 1)
-                {
-                    if (!MapMatrix[i,j].right)
-                    {
-                        GameObject rightWall = GameObject.Instantiate(_wall);
-                        rightWall.transform.position = rightLoc;
-                        rightWall.transform.parent = mapMaster.transform;
-                        rightWall.layer = 10;
-                    }
-                }
-                if(j == MapSize - 1)
-                {
-                    if (!MapMatrix[i,j].down)
-                    {
-                        GameObject downWall = GameObject.Instantiate(_wall);
-                        downWall.transform.position = downLoc;
-                        downWall.transform.rotation = Quaternion.Euler(0, -90, 0);
-                        downWall.transform.parent = mapMaster.transform;
-                        downWall.layer = 10;
-                    }
-                }
-                //move spawn locations 5 units over
-                upLoc.z += 5;
-                downLoc.z += 5;
-                rightLoc.z += 5;
-                leftLoc.z += 5;
-                floorLoc.z += 5;
             }
-            //reset spawn locations
-            upLoc.z = 0;
-            downLoc.z = 0;
-            rightLoc.z = 5;
-            leftLoc.z = 0;
-            floorLoc.z = 0;
-            //move spawn locations down
-            upLoc.x += 5;
-            downLoc.x += 5;
-            rightLoc.x += 5;
-            leftLoc.x += 5;
-            floorLoc.x += 5;
-            
+            wallSpawn.x += 5;
+            floorSpawn.x += 5;
+
+            wallSpawn.z = 2.5f;
+            floorSpawn.z = 0;
         }
     }
 
